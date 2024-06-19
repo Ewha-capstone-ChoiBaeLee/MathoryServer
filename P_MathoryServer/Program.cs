7097249dfcdeb265b526a52eb3f4f7fdb71b4821
princesschoco <90818568+princesschoco@users.noreply.github.com>
2024-06-19 15:51:29 +0900

    Update Program.cs

--- P_MathoryServer/Program.cs
+++ P_MathoryServer/Program.cs
@@ -12,15 +12,37 @@ builder.Services.AddScoped<StoryLineProcessingService>();
 
 builder.Services.AddHttpClient();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+
+// Use the connection string from the environment variable
+var connectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_AZURE_MYSQL_CONNECTIONSTRING");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))); // Ensure to use the appropriate MySQL server version
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
